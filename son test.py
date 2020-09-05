3048fa64923cd8f0a864ddccb4089fa98b376bc1
Cavid Hesenov <cavid.hasanov2@bk.ru>
2020-09-05 16:56:06 +0500

    Taskin helli yerlesdirildi

--- son test.py
+++ son test.py
@@ -34,11 +34,12 @@ print(is_leap(year))"""
 
 
 def is_leap(year):
-    if year % 4 == 0 or year / 400 == 0:
+    if year % 400 == 0:
         return "True"
     elif year % 100 == 0:
-        return ("False")
-
+        return "False"
+    if year % 4 == 0:
+        return "True"
     else:
         return ("False")
 
